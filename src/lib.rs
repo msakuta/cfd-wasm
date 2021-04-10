6cb74ab9eae8cf6c22c876dfdb8fe40ba2ad2dc3
msakuta <masahiro.sakuta@gmail.com>
2021-04-10 18:07:29 +0900

    First version of fluid simulation

--- src/lib.rs
+++ src/lib.rs
@@ -75,6 +75,14 @@ pub fn turing(width: usize, height: usize, callback: js_sys::Function) -> Result
     panic::set_hook(Box::new(console_error_panic_hook::hook));
     let mut u = vec![0f64; 4 * width * height];
     let mut v = vec![0f64; 4 * width * height];
+    let mut s = vec![0f64; 4 * width * height];
+    let mut density = vec![0f64; 4 * width * height];
+
+    let mut Vx = vec![0f64; 4 * width * height];
+    let mut Vy = vec![0f64; 4 * width * height];
+
+    let mut Vx0 = vec![0f64; 4 * width * height];
+    let mut Vy0 = vec![0f64; 4 * width * height];
     let mut data = vec![0u8; 4 * width * height];
 
     let mut xor128 = Xor128::new(123);
@@ -94,7 +102,7 @@ pub fn turing(width: usize, height: usize, callback: js_sys::Function) -> Result
         }
     }
 
-    let render = |height, width, data: &mut Vec<u8>, u: &mut Vec<f64>, v: &mut Vec<f64>| {
+    let render = |height, width, data: &mut Vec<u8>, u: &[f64], v: &[f64]| {
         for y in 0..height {
             for x in 0..width {
                 data[(x + y * width) * 4    ] = ((u[x + y * width]) / uMax * 127.) as u8;
@@ -116,7 +124,8 @@ pub fn turing(width: usize, height: usize, callback: js_sys::Function) -> Result
         rv: f64,
     }
 
-    let params = Rc::new(RefCell::new(Params{
+    let params = //Rc::new(RefCell::new(
+        Params{
         delta_time: 1.,
         skip_frames: 1,
         mouse_pos: [0, 0],
@@ -124,22 +133,196 @@ pub fn turing(width: usize, height: usize, callback: js_sys::Function) -> Result
         k: 0.056,
         ru: 0.07,
         rv: 0.056,
-    }));
+    };
 
-    fn diffuse(width: usize, height: usize, u_next: &mut Vec<f64>, u: &Vec<f64>, d: f64){
+
+    // fn diffuse(width: usize, height: usize, u_next: &mut Vec<f64>, u: &Vec<f64>, d: f64){
+    //     let [iwidth, iheight] = [width as isize, height as isize];
+    //     for y in 0..iheight {
+    //         for x in 0..iwidth {
+    //             u_next[x as usize + y as usize * width] += {
+    //                 let fu = |x, y| {
+    //                     u[((x + iwidth) % iwidth + (y + iheight) % iheight * iwidth) as usize]
+    //                 };
+    //                 d * (fu(x + 1, y) + fu(x - 1, y)
+    //                     + fu(x, y - 1) + fu(x, y + 1) - 4. * fu(x, y))
+    //             };
+    //         }
+    //     }
+    // }
+
+    fn add_density(density: &mut [f64], x: usize, y: usize, amount: f64, width: usize, height: usize) {
+        let ix = |x, y| {
+            ((x + width) % width + (y + height) % height * width) as usize
+        };
+        density[ix(x, y)] += amount;
+    }
+
+    fn add_velo(vx: &mut [f64], vy: &mut [f64], index: usize, amount: [f64; 2]) {
+        vx[index] += amount[0];
+        vy[index] += amount[1];
+    }
+
+    let set_bnd = |b: i32, x: &mut [f64], width: usize, height: usize| {
         let [iwidth, iheight] = [width as isize, height as isize];
-        for y in 0..iheight {
-            for x in 0..iwidth {
-                u_next[x as usize + y as usize * width] += {
-                    let fu = |x, y| {
-                        u[((x + iwidth) % iwidth + (y + iheight) % iheight * iwidth) as usize]
-                    };
-                    d * (fu(x + 1, y) + fu(x - 1, y)
-                        + fu(x, y - 1) + fu(x, y + 1) - 4. * fu(x, y))
-                };
+        let ix = |x, y| {
+            ((x + width) % width + (y + height) % height * width) as usize
+        };
+        // Edge cases
+        for i in 1..width - 1 {
+            x[ix(i, 0  )] = if b == 2 { -x[ix(i, 1  )] } else { x[ix(i, 1  )] };
+            x[ix(i, height-1)] = if b == 2 { -x[ix(i, height-2)] } else { x[ix(i, height-2)] };
+        }
+        for j in 1..height - 1 {
+            x[ix(0  , j)] = if b == 1 { -x[ix(1  , j)] } else { x[ix(1  , j)] };
+            x[ix(width-1, j)] = if b == 1 { -x[ix(width-2, j)] } else { x[ix(width-2, j)] };
+        }
+
+        // Corner cases (literally)
+        // x[ix!(0, 0)]       = 0.33f * (x[ix!(1, 0)]
+        //                             + x[ix!(0, 1)]);
+        // x[ix!(0, height-1)]     = 0.33f * (x[ix!(1, height-1, 0)]
+        //                             + x[ix!(0, N-2, 0)]
+        //                             + x[ix!(0, N-1, 1)]);
+        // x[ix!(0, 0)]     = 0.33f * (x[ix!(1, 0, N-1)]
+        //                             + x[ix!(0, 1, N-1)]
+        //                             + x[ix!(0, 0, N)]);
+        // x[ix!(0, N-1, N-1)]   = 0.33f * (x[ix!(1, N-1, N-1)]
+        //                             + x[ix!(0, N-2, N-1)]
+        //                             + x[ix!(0, N-1, N-2)]);
+        // x[ix!(N-1, 0, 0)]     = 0.33f * (x[ix!(N-2, 0, 0)]
+        //                             + x[ix!(N-1, 1, 0)]
+        //                             + x[ix!(N-1, 0, 1)]);
+        // x[ix!(N-1, N-1, 0)]   = 0.33f * (x[ix!(N-2, N-1, 0)]
+        //                             + x[ix!(N-1, N-2, 0)]
+        //                             + x[ix!(N-1, N-1, 1)]);
+        // x[ix!(N-1, 0, N-1)]   = 0.33f * (x[ix!(N-2, 0, N-1)]
+        //                             + x[ix!(N-1, 1, N-1)]
+        //                             + x[ix!(N-1, 0, N-2)]);
+        // x[ix!(N-1, N-1, N-1)] = 0.33f * (x[ix!(N-2, N-1, N-1)]
+        //                             + x[ix!(N-1, N-2, N-1)]
+        //                             + x[ix!(N-1, N-1, N-2)]);
+    };
+
+    fn lin_solve(b: i32, x: &mut [f64], x0: &[f64], a: f64, c: f64, iter: usize, width: usize, height: usize) {
+        let (iwidth, iheight) = (width as isize, height as isize);
+        let ix = |x, y| {
+            ((x + width) % width + (y + height) % height * width) as usize
+        };
+        let cRecip = 1.0 / c;
+        for k in 0..iter {
+            for j in 1..height - 1 {
+                for i in 1..width - 1 {
+                    x[ix(i, j)] =
+                        (x0[ix(i, j)]
+                            + a*(    x[ix(i+1, j  )]
+                                    +x[ix(i-1, j  )]
+                                    +x[ix(i  , j+1)]
+                                    +x[ix(i  , j-1)]
+                                    +x[ix(i  , j  )]
+                                    +x[ix(i  , j  )]
+                            )) * cRecip;
+                }
             }
+            // set_bnd(b, x, N);
         }
     }
+
+    fn diffuse(b: i32, x: &mut [f64], x0: &[f64], diff: f64, dt: f64, iter: usize, width: usize, height: usize) {
+        let a = dt * diff * (width - 2) as f64 * (height - 2) as f64;
+        lin_solve(b, x, x0, a, 1. + 6. * a, iter, width, height);
+    }
+
+    fn advect(b: i32, d: &mut [f64], d0: &[f64], velocX: &[f64], velocY: &[f64], dt: f64, width: usize, height: usize) {
+        let (iwidth, iheight) = (width as isize, height as isize);
+        let ix = |x, y| {
+            ((x + width) % width + (y + height) % height * width) as usize
+        };
+    
+        let dtx = dt * (width - 2) as f64;
+        let dty = dt * (height - 2) as f64;
+
+        let fwidth = width as f64;
+        let fheight = height as f64;
+
+        for j in 1..height - 1 { 
+            let jfloat = j as f64;
+            for i in 1..width - 1 {
+                let ifloat = i as f64;
+                let mut x    = ifloat - dtx * velocX[ix(i, j)];
+                let mut y    = jfloat - dty * velocY[ix(i, j)];
+                
+                if x < 0.5 { x = 0.5 }
+                if x > fwidth + 0.5 { x = fwidth + 0.5 };
+                let i0 = x.floor();
+                let i1 = i0 + 1.0;
+                if y < 0.5 { y = 0.5 }
+                if y > fheight + 0.5 { y = fheight + 0.5 };
+                let j0 = y.floor();
+                let j1 = j0 + 1.0; 
+                
+                let s1 = x - i0; 
+                let s0 = 1.0 - s1; 
+                let t1 = y - j0; 
+                let t0 = 1.0 - t1;
+                
+                let i0i = i0 as usize;
+                let i1i = i1 as usize;
+                let j0i = j0 as usize;
+                let j1i = j1 as usize;
+                
+                d[ix(i, j)] = 
+                    s0 * ( t0 * (d0[ix(i0i, j0i)])
+                        +( t1 * (d0[ix(i0i, j1i)])))
+                +s1 * ( t0 * (d0[ix(i1i, j0i)])
+                        +( t1 * (d0[ix(i1i, j1i)])));
+            }
+        }
+        // set_bnd(b, d, N);
+    }
+
+    struct State {
+        s: Vec<f64>,
+        density: Vec<f64>,
+        width: usize,
+        height: usize,
+        params: Params,
+    }
+
+    impl State {
+
+
+        fn fluid_step(&mut self, Vx: &mut [f64], Vy: &mut [f64]) {
+            let visc     = 1.0;
+            let diff     = 0.5;
+            let dt       = self.params.delta_time;
+            let mut Vx0     = Vx.to_vec();
+            let mut Vy0     = Vy.to_vec();
+            let s       = &mut self.s;
+            let density = &mut self.density;
+            
+            diffuse(1, &mut Vx0, Vx, visc, dt, 4, self.width, self.height);
+            diffuse(2, &mut Vy0, Vy, visc, dt, 4, self.width, self.height);
+            
+            // project(Vx0, Vy0, Vz0, Vx, Vy, 4, N);
+            
+            advect(1, Vx, &Vx0, &Vx0, &Vy0, dt, self.width, self.height);
+            advect(2, Vy, &Vy0, &Vx0, &Vy0, dt, self.width, self.height);
+            
+            // project(Vx, Vy, Vz, Vx0, Vy0, 4, N);
+            
+            diffuse(0, s, density, diff, dt, 4, self.width, self.height);
+            advect(0, density, s, Vx, Vy, dt, self.width, self.height);
+        }
+    }
+
+    let mut state = State{
+        s,
+        density,
+        width,
+        height,
+        params,
+    };
 
     fn react_u(width: usize, height: usize, u_next: &mut Vec<f64>, u: &Vec<f64>, v: &Vec<f64>, params: &Params){
         for y in 0..height {
@@ -179,24 +362,32 @@ pub fn turing(width: usize, height: usize, callback: js_sys::Function) -> Result
     console_log!("Starting frames");
 
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        let params_val = *(*params).borrow();
-        let Params{delta_time, f, k, mouse_pos, ..} = params_val;
+        let Params{delta_time, f, k, mouse_pos, ..} = state.params;
         console_log!("Rendering frame {}, mouse_pos: {}, {} delta_time: {}, skip_frames: {}, f: {}, k: {}, ru: {}, rv: {}",
-            i, mouse_pos[0], mouse_pos[1], delta_time, params_val.skip_frames, f, k, params_val.ru, params_val.rv);
+            i, mouse_pos[0], mouse_pos[1], delta_time, state.params.skip_frames, f, k, state.params.ru, state.params.rv);
 
         i += 1;
 
-        for _ in 0..params_val.skip_frames {
-            let mut u_next = u.clone();
-            let mut v_next = v.clone();
-            diffuse(width, height, &mut u_next, &u, &params_val.rv * delta_time);
-            diffuse(width, height, &mut v_next, &v, &params_val.ru * delta_time);
-            react_u(width, height, &mut u_next, &u, &v, &params_val);
-            react_v(width, height, &mut v_next, &u, &v, &params_val);
+        let (iwidth, iheight) = (width as isize, height as isize);
+        let ix = |x, y| {
+            ((x as isize + iwidth) % iwidth + (y as isize + iheight) % iheight * iwidth) as usize
+        };
+
+        add_density(&mut state.density, state.width / 2, state.height / 2, 1., state.width, state.height);
+        add_velo(&mut Vx, &mut Vy, ix(mouse_pos[0], mouse_pos[1]), [1., 1.]);
+
+        for _ in 0..state.params.skip_frames {
+            state.fluid_step(&mut Vx, &mut Vy);
+            // let mut u_next = u.clone();
+            // let mut v_next = v.clone();
+            // diffuse(width, height, &mut u_next, &u, &params_val.rv * delta_time);
+            // diffuse(width, height, &mut v_next, &v, &params_val.ru * delta_time);
+            // react_u(width, height, &mut u_next, &u, &v, &params_val);
+            // react_v(width, height, &mut v_next, &u, &v, &params_val);
             // clip(width, height, &mut u, uMax, 0.);
             // clip(width, height, &mut v, vMax, 0.);
-            u = u_next;
-            v = v_next;
+            // u = u_next;
+            // v = v_next;
         }
 
         let [xx, yy] = mouse_pos;
@@ -218,7 +409,7 @@ pub fn turing(width: usize, height: usize, callback: js_sys::Function) -> Result
             accum / (width * height) as f64
         };
 
-        render(height, width, &mut data, &mut u, &mut v);
+        render(height, width, &mut data, &mut state.density, &mut v);
 
         let image_data = web_sys::ImageData::new_with_u8_clamped_array_and_sh(wasm_bindgen::Clamped(&mut data), width as u32, height as u32).unwrap();
 
@@ -229,12 +420,13 @@ pub fn turing(width: usize, height: usize, callback: js_sys::Function) -> Result
         if terminate_requested.is_truthy() {
             return
         }
-        let assign_state = |name: &str, setter: fn(params: &mut Params, val: f64)| {
+        let mut assign_state = |name: &str, setter: fn(params: &mut Params, val: f64)| {
             if let Ok(new_val) = js_sys::Reflect::get(&callback_ret, &JsValue::from(name)) {
                 if let Some(the_val) = new_val.as_f64() {
                     // console_log!("received {} value: {}", name, the_val);
-                    let mut params_ref = (*params).borrow_mut();
-                    setter(&mut params_ref, the_val);
+                    // let mut params_ref = (*params).borrow_mut();
+                    // setter(&mut params_ref, the_val);
+                    setter(&mut state.params, the_val);
                 }
             }
         };
@@ -249,7 +441,7 @@ pub fn turing(width: usize, height: usize, callback: js_sys::Function) -> Result
                 if let Ok(the_val) = js_sys::Reflect::get_u32(&new_val, i) {
                     if let Some(value) = the_val.as_f64() {
                         console_log!("received mouse_pos[{}] value: {}", i, value);
-                        let mut params_ref = (*params).borrow_mut();
+                        let mut params_ref = state.params;
                         params_ref.mouse_pos[i as usize] = value as i32;
                     }
                 }
